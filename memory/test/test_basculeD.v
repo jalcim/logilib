e870dbaabfe496e3e4d79118052b00bfd58a23bd
ghost <ghost@debian>
2021-01-30 00:39:12 +0100

    formal test ok

--- memory/test/test_basculeD.v
+++ memory/test/test_basculeD.v
@@ -5,135 +5,218 @@ module test_basculeD;
    basculeD basculeD(a, clk, reset, s1, s2);
 
    integer output_file;
+   reg 	   state;
    initial
      begin
 	output_file = $fopen("build/memory/log/outfile_test_basculeD");
 	$dumpfile("build/memory/signal/signal_test_basculeD.vcd");
 	$dumpvars;
 	$display("basculeD");
-	$display("\t\ttime,\ta,\tclk,\treset,\ts1,\ts2");
-	$monitor("%d\t%b\t%b\t%b\t%b\t%b", $time, a, clk, reset, s1, s2);
+	$display("\t\ttime,\ta,\tclk,\treset,\ts1,\ts2,\tstate");
+	$monitor("%d\t%b\t%b\t%b\t%b\t%b\t%b", $time, a, clk, reset, s1, s2, state);
 
-	a = 0;
-	clk = 0;
-	reset = 1;
-	#5;
-	if (!s1 && s2)
-	  begin
-	     $fdisplay(output_file, "ok");
-	  end
-	else
-	  begin
-	     $fdisplay(output_file, "fail");
-	  end
-	#5;
-	a = 0;
-	clk = 0;
-	reset = 0;
-	#5;
-	if (!s1 && s2)
-	  begin
-	     $fdisplay(output_file, "ok");
-	  end
-	else
-	  begin
-	     $fdisplay(output_file, "fail");
-	  end
-	#5;
-	a = 1;
-	clk = 0;
-	reset = 0;
-	#5;
-	if (!s1 && s2)
-	  begin
-	     $fdisplay(output_file, "ok");
-	  end
-	else
-	  begin
-	     $fdisplay(output_file, "fail");
-	  end
-	#5;
-	a = 0;
-	clk = 1;
-	reset = 0;
-	#5;
-	if (!s1 && s2)
-	  begin
-	     $fdisplay(output_file, "ok");
-	  end
-	else
-	  begin
-	     $fdisplay(output_file, "fail");
-	  end
-	#5;
-	a = 1;
-	clk = 1;
-	reset = 0;
-	#5;
-	if (s1 && !s2)
-	  begin
-	     $fdisplay(output_file, "ok");
-	  end
-	else
-	  begin
-	     $fdisplay(output_file, "fail");
-	  end
-	#5;
-	a = 0;
-	clk = 0;
-	reset = 0;
-	#5;
-	if (s1 && !s2)
-	  begin
-	     $fdisplay(output_file, "ok");
-	  end
-	else
-	  begin
-	     $fdisplay(output_file, "fail");
-	  end
-	#5;
-	a = 1;
-	clk = 0;
-	reset = 0;
-	#5;
-	if (s1 && !s2)
-	  begin
-	     $fdisplay(output_file, "ok");
-	  end
-	else
-	  begin
-	     $fdisplay(output_file, "fail");
-	  end
-	#5;
-	a = 0;
-	clk = 1;
-	reset = 0;
-	#5;
-	if (!s1 && s2)
-	  begin
-	     $fdisplay(output_file, "ok");
-	  end
-	else
-	  begin
-	     $fdisplay(output_file, "fail");
-	  end
-	#5;
-	a = 0;
-	clk = 0;
-	reset = 0;
-	#5;
-	if (!s1 && s2)
-	  begin
-	     $fdisplay(output_file, "ok");
-	  end
-	else
-	  begin
-	     $fdisplay(output_file, "fail");
-	  end
+	state = 0;
 	#5;
 	a = 0;
 	clk = 0;
 	reset = 1;
+	if (~state & (a & clk))
+	  begin
+	     state = 1;
+	  end
+	else if (state & (~a & clk))
+	  begin
+	     state = 0;
+	  end
+	#5;
+	if (!s1 && s2)
+	  begin
+	     $fdisplay(output_file, "ok");
+	  end
+	else
+	  begin
+	     $fdisplay(output_file, "fail");
+	  end
+	#5;
+	a = 0;
+	clk = 0;
+	reset = 0;
+	if (~state & (a & clk))
+	  begin
+	     state = 1;
+	  end
+	else if (state & (~a & clk))
+	  begin
+	     state = 0;
+	  end
+	#5;
+	if (!s1 && s2)
+	  begin
+	     $fdisplay(output_file, "ok");
+	  end
+	else
+	  begin
+	     $fdisplay(output_file, "fail");
+	  end
+	#5;
+	a = 1;
+	clk = 0;
+	reset = 0;
+	if (~state & (a & clk))
+	  begin
+	     state = 1;
+	  end
+	else if (state & (~a & clk))
+	  begin
+	     state = 0;
+	  end
+	#5;
+	if (!s1 && s2)
+	  begin
+	     $fdisplay(output_file, "ok");
+	  end
+	else
+	  begin
+	     $fdisplay(output_file, "fail");
+	  end
+	#5;
+	a = 0;
+	clk = 1;
+	reset = 0;
+	if (~state & (a & clk))
+	  begin
+	     state = 1;
+	  end
+	else if (state & (~a & clk))
+	  begin
+	     state = 0;
+	  end
+	#5;
+	if (!s1 && s2)
+	  begin
+	     $fdisplay(output_file, "ok");
+	  end
+	else
+	  begin
+	     $fdisplay(output_file, "fail");
+	  end
+	#5;
+	a = 1;
+	clk = 1;
+	reset = 0;
+	if (~state & (a & clk))
+	  begin
+	     state = 1;
+	  end
+	else if (state & (~a & clk))
+	  begin
+	     state = 0;
+	  end
+	#5;
+	if (s1 && !s2)
+	  begin
+	     $fdisplay(output_file, "ok");
+	  end
+	else
+	  begin
+	     $fdisplay(output_file, "fail");
+	  end
+	#5;
+	a = 0;
+	clk = 0;
+	reset = 0;
+	if (~state & (a & clk))
+	  begin
+	     state = 1;
+	  end
+	else if (state & (~a & clk))
+	  begin
+	     state = 0;
+	  end
+	#5;
+	if (s1 && !s2)
+	  begin
+	     $fdisplay(output_file, "ok");
+	  end
+	else
+	  begin
+	     $fdisplay(output_file, "fail");
+	  end
+	#5;
+	a = 1;
+	clk = 0;
+	reset = 0;
+	if (~state & (a & clk))
+	  begin
+	     state = 1;
+	  end
+	else if (state & (~a & clk))
+	  begin
+	     state = 0;
+	  end
+	#5;
+	if (s1 && !s2)
+	  begin
+	     $fdisplay(output_file, "ok");
+	  end
+	else
+	  begin
+	     $fdisplay(output_file, "fail");
+	  end
+	#5;
+	a = 0;
+	clk = 1;
+	reset = 0;
+	if (~state & (a & clk))
+	  begin
+	     state = 1;
+	  end
+	else if (state & (~a & clk))
+	  begin
+	     state = 0;
+	  end
+	#5;
+	if (!s1 && s2)
+	  begin
+	     $fdisplay(output_file, "ok");
+	  end
+	else
+	  begin
+	     $fdisplay(output_file, "fail");
+	  end
+	#5;
+	a = 0;
+	clk = 0;
+	reset = 0;
+	if (~state & (a & clk))
+	  begin
+	     state = 1;
+	  end
+	else if (state & (~a & clk))
+	  begin
+	     state = 0;
+	  end
+	#5;
+	if (!s1 && s2)
+	  begin
+	     $fdisplay(output_file, "ok");
+	  end
+	else
+	  begin
+	     $fdisplay(output_file, "fail");
+	  end
+	#5;
+	a = 0;
+	clk = 0;
+	reset = 1;
+	if (~state & (a & clk))
+	  begin
+	     state = 1;
+	  end
+	else if (state & (~a & clk))
+	  begin
+	     state = 0;
+	  end
 	#5;
 	if (!s1 && s2)
 	  begin
