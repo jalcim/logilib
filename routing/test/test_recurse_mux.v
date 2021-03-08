7724cce75c5b614c665d5dcbdc69f83a3080ec2a
ghost <alcim.dev@gmail.com>
2021-03-08 22:46:34 +0100

    rework recurse_mux.v

--- routing/test/test_recurse_mux.v
+++ routing/test/test_recurse_mux.v
@@ -2,36 +2,36 @@ module test_recurse_mux;
    parameter S = 2;
    parameter T = 1;
 
-   wire [T - 1 : 0] z;
-   reg [2 ** S - 1 : 0] d;
-   reg [1 : 0] s;
+   wire [T - 1 : 0] out;
+   reg [2 ** S - 1 : 0] in;
+   reg [1 : 0] ctrl;
 
-   recurse_mux #(.S(S), .T(T))mux0(z, d, s);
+   recurse_mux #(.S(S), .T(T))mux0(ctrl, in, out);
 
    initial
      begin
 	$dumpfile("build/routing/signal/signal_recurse_mux.vcd");
 	$dumpvars;
-	$display("\t\ttime,\tz, \td[0],\td[1], \td[2], \td[3],\ts");
-	$monitor("%d \t%b \t%b \t%b \t%b \t%b \t%d", $time, z, d[0], d[1], d[2], d[3], s);
+	$display("\t\ttime,\tout, \tin[0],\tin[1], \tin[2], \tin[3],\tctrl");
+	$monitor("%d \t%b \t%b \t%b \t%b \t%b \t%d", $time, out, in[0], in[1], in[2], in[3], ctrl);
 
 	
-	d[0] = 1;
-	d[1] = 0;
-	d[2] = 1;
-	d[3] = 0;
+	in[0] = 1;
+	in[1] = 0;
+	in[2] = 1;
+	in[3] = 0;
 	
-	s[0] = 0;
-	s[1] = 0;
+	ctrl[0] = 0;
+	ctrl[1] = 0;
 	#5;
-	s[0] = 1;
-	s[1] = 0;
+	ctrl[0] = 1;
+	ctrl[1] = 0;
 	#5;
-	s[0] = 0;
-	s[1] = 1;
+	ctrl[0] = 0;
+	ctrl[1] = 1;
 	#5;
-	s[0] = 1;
-	s[1] = 1;
+	ctrl[0] = 1;
+	ctrl[1] = 1;
 	#5;
      end
 endmodule
--- routing/test/test_recurse_mux8.v
+++ routing/test/test_recurse_mux8.v
@@ -2,41 +2,41 @@ module test_recurse_mux;
    parameter S = 2;
    parameter T = 8;
 
-   wire [T - 1 : 0] z;
-   reg [(2 ** S) * T - 1 : 0] d;
-   reg [S-1 : 0] s;
+   wire [T - 1 : 0] out;
+   reg [(2 ** S) * T - 1 : 0] in;
+   reg [S-1 : 0] ctrl;
 
    integer     cpt;
    reg 	       xin;
 
-   recurse_mux #(.S(S), .T(T)) mux0(z, d, s);
+   recurse_mux #(.S(S), .T(T)) mux0(ctrl, in, out);
 
    initial
      begin
 	$dumpfile("build/routing/signal/signal_recurse_mux8.vcd");
 	$dumpvars;
-	$display("\t\ttime,\tz,\ts");
-	$monitor("%d\t%d\t%d", $time, z[7:0], s[1:0]);
+	$display("\t\ttime,\tout,\ts");
+	$monitor("%d\t%d\t%d", $time, out[7:0], ctrl[1:0]);
 
 	cpt = -1;
 	xin = 0;
 	while (++cpt <= (2**S) * T - 1)
 	  begin
-	     d[cpt] = xin;
+	     in[cpt] = xin;
 	     xin = ~xin;
 	  end  
 
-	s[0] = 0;
-	s[1] = 0;
+	ctrl[0] = 0;
+	ctrl[1] = 0;
 	#5;
-	s[0] = 1;
-	s[1] = 0;
+	ctrl[0] = 1;
+	ctrl[1] = 0;
 	#5;
-	s[0] = 0;
-	s[1] = 1;
+	ctrl[0] = 0;
+	ctrl[1] = 1;
 	#5;
-	s[0] = 1;
-	s[1] = 1;
+	ctrl[0] = 1;
+	ctrl[1] = 1;
 	#5;
      end
 endmodule
