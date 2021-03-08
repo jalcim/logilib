4a636c5a4dc28cb718f1291aa9a6fc08d8ead755
ghost <alcim.dev@gmail.com>
2021-03-08 19:08:36 +0100

    rework of gate8

--- primitive/src/gate8.v
+++ primitive/src/gate8.v
@@ -1,15 +1,31 @@
+module gate_buf8(e1, s);
+   input [7:0] e1;
+   output [7:0] s;
+
+   gate_buf buf0(e1[0], s[0]);
+   gate_buf buf1(e1[1], s[1]);
+   gate_buf buf2(e1[2], s[2]);
+   gate_buf buf3(e1[3], s[3]);
+   gate_buf buf4(e1[4], s[4]);
+   gate_buf buf5(e1[5], s[5]);
+   gate_buf buf6(e1[6], s[6]);
+   gate_buf buf7(e1[7], s[7]);
+
+endmodule   
+
 module gate_not8 (e1, s);
    input [7:0] e1;
    output [7:0] s;
 
-   assign s[0] = ~e1[0];
-   assign s[1] = ~e1[1];
-   assign s[2] = ~e1[2];
-   assign s[3] = ~e1[3];
-   assign s[4] = ~e1[4];
-   assign s[5] = ~e1[5];
-   assign s[6] = ~e1[6];
-   assign s[7] = ~e1[7];
+   gate_not not0(e1[0], s[0]);
+   gate_not not1(e1[1], s[1]);
+   gate_not not2(e1[2], s[2]);
+   gate_not not3(e1[3], s[3]);
+   gate_not not4(e1[4], s[4]);
+   gate_not not5(e1[5], s[5]);
+   gate_not not6(e1[6], s[6]);
+   gate_not not7(e1[7], s[7]);
+
 endmodule // not
 
 module gate_and8 (e1, e2, s);
@@ -25,70 +41,78 @@ module gate_and8 (e1, e2, s);
    gate_and and6(e1[6], e2[6], s[6]);
    gate_and and7(e1[7], e2[7], s[7]);
    
-/*
-   assign s[0] = e1[0] & e2[0];
-   assign s[1] = e1[1] & e2[1];
-   assign s[2] = e1[2] & e2[2];
-   assign s[3] = e1[3] & e2[3];
-   assign s[4] = e1[4] & e2[4];
-   assign s[5] = e1[5] & e2[5];
-   assign s[6] = e1[6] & e2[6];
-   assign s[7] = e1[7] & e2[7];
- */
 endmodule // and
 
 module gate_or8 (e1, e2, s);
    input [7:0] e1, e2;
    output [7:0] s;
 
-   assign s[0] = e1[0] | e2[0];
-   assign s[1] = e1[1] | e2[1];
-   assign s[2] = e1[2] | e2[2];
-   assign s[3] = e1[3] | e2[3];
-   assign s[4] = e1[4] | e2[4];
-   assign s[5] = e1[5] | e2[5];
-   assign s[6] = e1[6] | e2[6];
-   assign s[7] = e1[7] | e2[7];
+   gate_or or0(e1[0], e2[0], s[0]);
+   gate_or or1(e1[1], e2[1], s[1]);
+   gate_or or2(e1[2], e2[2], s[2]);
+   gate_or or3(e1[3], e2[3], s[3]);
+   gate_or or4(e1[4], e2[4], s[4]);
+   gate_or or5(e1[5], e2[5], s[5]);
+   gate_or or6(e1[6], e2[6], s[6]);
+   gate_or or7(e1[7], e2[7], s[7]);
 endmodule // or
 
 module gate_xor8 (e1, e2, s);
    input [7:0] e1, e2;
    output [7:0] s;
+
+   gate_xor xor0(e1[0], e2[0], s[0]);
+   gate_xor xor1(e1[1], e2[1], s[1]);
+   gate_xor xor2(e1[2], e2[2], s[2]);
+   gate_xor xor3(e1[3], e2[3], s[3]);
+   gate_xor xor4(e1[4], e2[4], s[4]);
+   gate_xor xor5(e1[5], e2[5], s[5]);
+   gate_xor xor6(e1[6], e2[6], s[6]);
+   gate_xor xor7(e1[7], e2[7], s[7]);
+
+endmodule // xor
+
+module gate_xnor8 (e1, e2, s);
+   input [7:0] e1, e2;
+   output [7:0] s;
  
-   assign s[0] = e1[0] ^ e2[0];
-   assign s[1] = e1[1] ^ e2[1];
-   assign s[2] = e1[2] ^ e2[2];
-   assign s[3] = e1[3] ^ e2[3];
-   assign s[4] = e1[4] ^ e2[4];
-   assign s[5] = e1[5] ^ e2[5];
-   assign s[6] = e1[6] ^ e2[6];
-   assign s[7] = e1[7] ^ e2[7];
+   gate_xnor xnor0(e1[0], e2[0], s[0]);
+   gate_xnor xnor1(e1[1], e2[1], s[1]);
+   gate_xnor xnor2(e1[2], e2[2], s[2]);
+   gate_xnor xnor3(e1[3], e2[3], s[3]);
+   gate_xnor xnor4(e1[4], e2[4], s[4]);
+   gate_xnor xnor5(e1[5], e2[5], s[5]);
+   gate_xnor xnor6(e1[6], e2[6], s[6]);
+   gate_xnor xnor7(e1[7], e2[7], s[7]);
+
 endmodule // xor
 
 module gate_nand8 (e1, e2, s);
    input [7:0] e1, e2;
    output [7:0] s;
 
-   assign s[0] = ~(e1[0] & e2[0]);
-   assign s[1] = ~(e1[1] & e2[1]);
-   assign s[2] = ~(e1[2] & e2[2]);
-   assign s[3] = ~(e1[3] & e2[3]);
-   assign s[4] = ~(e1[4] & e2[4]);
-   assign s[5] = ~(e1[5] & e2[5]);
-   assign s[6] = ~(e1[6] & e2[6]);
-   assign s[7] = ~(e1[7] & e2[7]);
+   gate_nand nand0(e1[0], e2[0], s[0]);
+   gate_nand nand1(e1[1], e2[1], s[1]);
+   gate_nand nand2(e1[2], e2[2], s[2]);
+   gate_nand nand3(e1[3], e2[3], s[3]);
+   gate_nand nand4(e1[4], e2[4], s[4]);
+   gate_nand nand5(e1[5], e2[5], s[5]);
+   gate_nand nand6(e1[6], e2[6], s[6]);
+   gate_nand nand7(e1[7], e2[7], s[7]);
+
 endmodule // nand
   
 module gate_nor8 (e1, e2, s);
    input [7:0] e1, e2;
    output [7:0] s;
 
-   assign s[0] = ~(e1[0] | e2[0]);
-   assign s[1] = ~(e1[1] | e2[1]);
-   assign s[2] = ~(e1[2] | e2[2]);
-   assign s[3] = ~(e1[3] | e2[3]);
-   assign s[4] = ~(e1[4] | e2[4]);
-   assign s[5] = ~(e1[5] | e2[5]);
-   assign s[6] = ~(e1[6] | e2[6]);
-   assign s[7] = ~(e1[7] | e2[7]);
+   gate_nor nor0(e1[0], e2[0], s[0]);
+   gate_nor nor1(e1[1], e2[1], s[1]);
+   gate_nor nor2(e1[2], e2[2], s[2]);
+   gate_nor nor3(e1[3], e2[3], s[3]);
+   gate_nor nor4(e1[4], e2[4], s[4]);
+   gate_nor nor5(e1[5], e2[5], s[5]);
+   gate_nor nor6(e1[6], e2[6], s[6]);
+   gate_nor nor7(e1[7], e2[7], s[7]);
+
 endmodule // nor
