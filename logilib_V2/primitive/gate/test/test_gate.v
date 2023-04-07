1ca0402ac4b0c2fcd98c033ebb3a11db0d1aba0c
jalcim <jalcim@DESKTOP-T8JSP1G.localdomain>
2023-04-07 12:29:02 +0200

    xnor

--- logilib_V2/primitive/gate/test/test_gate.v
+++ logilib_V2/primitive/gate/test/test_gate.v
@@ -1,6 +1,6 @@
 module test_gate;
    reg e1, e2;
-   wire out_not, out_nor, out_nand, out_and, out_or, out_xor;
+   wire out_not, out_nor, out_nand, out_and, out_or, out_xor, out_xnor;
 
    gate_not  inst0(out_not ,  e1);
    gate_nor  inst1(out_nor , {e1, e2});
@@ -8,14 +8,15 @@ module test_gate;
    gate_and  inst3(out_and , {e1, e2});
    gate_or   inst4(out_or  , {e1, e2});
    gate_xor  inst5(out_xor , {e1, e2});
+   gate_xnor inst6(out_xnor , {e1, e2});
 
    initial
      begin
 	$dumpfile("signal_test_parallel_nmos.vcd");
         $dumpvars;
-        $monitor("time %d\nnot\t%b\nnor\t%b\nnand\t%b\nand\t%b\nor\t%b\nxor\t%b\ne1\t%b\ne2\t%b\n",
+        $monitor("time %d\nnot\t%b\nnor\t%b\nnand\t%b\nand\t%b\nor\t%b\nxor\t%b\nxnor\t%b\ne1\t%b\ne2\t%b\n",
 		 $time, out_not, out_nor, out_nand,
-		 out_and, out_or, out_xor,
+		 out_and, out_or, out_xor, out_xnor,
 		 e1, e2);
 
 	e1 <= 0;
