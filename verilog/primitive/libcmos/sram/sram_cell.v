daeda8c8893d9b38435e500b7b1ffbe141bc8a5e
jalcim <jalcim@DESKTOP-T8JSP1G.localdomain>
2023-04-19 19:34:05 +0200

    reorganisation start

---
+++ verilog/primitive/libcmos/sram/sram_cell.v
@@ -0,0 +1,5 @@
+module sram_cell(data_out, data_in, sw);
+   input [1:0] data_in;
+   input       sw;
+   output      data_out;
+   
---
+++ verilog/primitive/serial_gate/test/test_serial_xor.v
@@ -0,0 +1,35 @@
+module _test_serial_xor;
+   parameter SIZE = 3;
+
+   reg [SIZE-1:0] e1;
+   wire out;
+   reg	clk;
+   integer i;
+
+   serial_xor #(.SIZE(SIZE)) test_serial_xor(out, e1);
+
+   initial
+     begin
+	$dumpfile("signal_serial_xor.vcd");
+        $dumpvars;
+        $display("\t\ttime, \te1, \tout");
+        $monitor("%d \t%b\t%b", $time, e1, out);
+
+	e1 <= 0;
+	#20;
+	e1 <= 1;
+	#20;
+	e1 <= 2;
+	#20;
+	e1 <= 3;
+	#20;
+	e1 <= 4;
+	#20;
+	e1 <= 5;
+	#20;
+	e1 <= 6;
+	#20;
+	e1 <= 7;
+	#20;
+     end
+endmodule
