0e7893b84984ec5300501b6c98ed2d101c57e54d
jalcim <jalcim@DESKTOP-T8JSP1G.localdomain>
2023-05-25 20:57:52 +0200

    clean

--- verilog/primitive/libcmos/sram/sram_cell.v
+++ verilog/primitive/libcmos/sram/sram_cell.v
@@ -1,5 +1,15 @@
 module sram_cell(data_out, data_in, sw);
-   input [1:0] data_in;
-   input       sw;
-   output      data_out;
+   input  data_in;
+   input  select;
+   output data_out;
    
+   wire   invL, invR;
+
+   gate_not inv_left  (invL, invR);
+   gate_not inv_right (invR, invL);   
+
+   t_nmos nmos_left  (invL, data_in, select);
+   t_nmos nmos_right (invR, data_in, select);   
+
+   assign data_out = invL;
+endmodule
