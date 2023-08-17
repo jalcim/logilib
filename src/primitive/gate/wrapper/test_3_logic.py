1a71802378a6985e1cd5a1ac2ee30868b73c9182
alcim jeremy <alcim.dev@gmail.com>
2023-08-17 09:43:48 +0200

    python POC fonctionnel

--- src/primitive/gate/wrapper/test_3_logic.py
+++ src/primitive/gate/wrapper/test_3_logic.py
@@ -12,12 +12,11 @@ from amaranth.back.rtlil import convert_fragment
 from collections import defaultdict, OrderedDict
 
 class Verilog_module(am.Elaboratable):
-    def __init__(self, name, **kwargs):
+    def __init__(self, name, id, **kwargs):
         self.kwargs = kwargs
-
-
-        # module_name, param, pin_in, pin_out, pin_in_out):
         self.name = name
+        self.rtlil_source = "" #affectation dans write_rtlil_file
+        self.rtlil_file = self.name + id + ".rtlil"
 
         self.params = []
         for key in kwargs.keys():
@@ -30,9 +29,6 @@ class Verilog_module(am.Elaboratable):
             if key[0:2] in ["i_", "o_"] or key[0:3] in ["io_"]:
                 print("PORT:", key)
                 self.ports.append(kwargs.get(key))
-
-        self.rtlil_source = "" # TODO
-        self.rtlil_file = self.name + ".rtlil"
 
     def elaborate(self, platform):
         m = Module()
@@ -47,57 +43,42 @@ class Regroupement():
 
     def __init__(self):
 
-        # self.param = [2, 2, 2, 2, 2, 2, None, None]
-        pin_out = Signal(8)
-        pin_in = [Signal(8), Signal(6)]
-        # pin_in_out = [Signal(8), Signal(6)]
-
-        """
-        self.modules = [
-            Verilog_module("gate_and"  , self.param[0], self.out[0], [self.pin_in[0][0], self.pin_in[1][0]], None),
-            Verilog_module("gate_nand" , self.param[1], self.out[1], [self.pin_in[0][1], self.pin_in[1][1]], None),
-            Verilog_module("gate_or"   , self.param[2], self.out[2], [self.pin_in[0][2], self.pin_in[1][2]], None),
-            Verilog_module("gate_nor"  , self.param[3], self.out[3], [self.pin_in[0][3], self.pin_in[1][3]], None),
-            Verilog_module("gate_xor"  , self.param[4], self.out[4], [self.pin_in[0][4], self.pin_in[1][4]], None),
-            Verilog_module("gate_xnor" , self.param[5], self.out[5], [self.pin_in[0][5], self.pin_in[1][5]], None),
-            Verilog_module("gate_not"  , self.param[6], self.out[6], self.pin_in[0][6], None),
-            Verilog_module("gate_buf"  , self.param[7], self.out[7], self.pin_in[0][7], None),
-        ]
-        """
-
-        nom_variable = "WIDTH"
+        nom_param = "SIZE"
+        value_param = 2
         self.modules = [
 
+            Verilog_module(
+                "gate_not",
+                "0", #attribuer un identifiant unique automatiquement
+                **{
+                    "i_in" : Signal(1),
+                    "o_out" : Signal(1)
+                }
+            ),
+            Verilog_module(
+                "gate_and",
+                "1",
+                **{
+                    "p_" + nom_param : value_param,#"p_WIDTH": self.width,
+                    "i_in" : Signal(value_param),
+                    "o_out" : Signal(1)
+                }
+            ),
+            Verilog_module(
+                "gate_and",
+                "2",
+                **{
+                    "p_SIZE": value_param,
+                    "i_in"  : Signal(value_param),
+                    "o_out" : Signal(1)
+                }
+            ),
             Verilog_module(
                 "gate_and",
                 **{
-                    "p_" + nom_variable : 8,#"p_WIDTH": self.width,
-                    "i_clk": Signal(1),
-                    "o_cnt" : Signal(8)
-                }
-            ),
-            Verilog_module(
-                "gate_nand",
-                **{
-                    "p_SIZE": 8,
-                    "i_clk": Signal(1),
-                    "o_cnt" : Signal(8)
-                }
-            ),
-            Verilog_module(
-                "gate_or",
-                **{
-                    "p_WIDTH": 8,
-                    "i_clk": Signal(1),
-                    "o_cnt" : Signal(8),
-                }
-            ),
-            Verilog_module(
-                "gate_nor",
-                **{
-                    "p_POUET": 8,
-                    "i_clk": Signal(1),
-                    "io_in_out" : Signal(8)
+                    "p_" + "SIZE": value_param,
+                    "i_in" : Signal(value_param),
+                    "o_out" : Signal(1)
                 }
             )
         ]
