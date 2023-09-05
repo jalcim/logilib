4472295d811d0be4e9e2a2cf294f214797949450
alcim jeremy <alcim.dev@gmail.com>
2023-09-05 08:03:59 +0200

    parallel_Dlatch multi clk

--- src/memory/latch/Dlatch/src/parallel_Dlatch.v
+++ src/memory/latch/Dlatch/src/parallel_Dlatch.v
@@ -5,18 +5,18 @@ module parallel_Dlatch(D, clk, Q, QN);
    parameter WAY = 3;
    parameter SIZE = 8;
 
-   input clk;
+   input [WAY-1:0] clk;
    input  [(WAY*SIZE)-1 : 0] D;
    output [(WAY*SIZE)-1 : 0] Q, QN;
 
    if (SIZE == 1)
-     Dlatch Dlatch0(D[0], clk, Q[0], QN[0]);
+     Dlatch Dlatch0(D[0], clk[0], Q[0], QN[0]);
    else if (SIZE > 1)
-     serial_Dlatch #(.SIZE(SIZE)) Dlatch1(data[SIZE-1:0], clk, Q[SIZE-1:0], QN[SIZE-1:0]);
+     serial_Dlatch #(.SIZE(SIZE)) Dlatch1(data[SIZE-1:0], clk[0], Q[SIZE-1:0], QN[SIZE-1:0]);
      
    if (WAY > 1)
      parallel_Dlatch #(.WAY(WAY-1), .SIZE(SIZE)) parallel_Dlatch0(D[WAY*SIZE-1 : SIZE],
-								  clk,
+								  clk[WAY-1:1],
 								  Q[WAY*SIZE-1 : SIZE],
 								  QN[WAY*SIZE-1 : SIZE]);
 endmodule
