959322daf2e80f70273daa7411a0a3a752360ad0
alcim jeremy <alcim.dev@gmail.com>
2023-09-06 16:25:48 +0200

    exemple debut implem blockreg

--- exemple/block_mem/v2/blockreg.v
+++ exemple/block_mem/v2/blockreg.v
@@ -10,21 +10,24 @@ module blockreg(clk,
 		datain,
 		data_out_A, data_out_B);
 
+   localparam NB_REG = 32;
+   localparam SIZE_REG = 32;
+   localparam SIZE = NB_REG * SIZE_REG;
+
    input clk, reset, write;
    input [4:0] write_reg, read_A, read_B;
-   input [31:0] datain;
+   input [SIZE_REG:0] datain;
 
-   output [31:0] data_out_A, data_out_B;
+   output [SIZE_REG-1:0] data_out_A, data_out_B;
 
    wire out_write_demux;
+   wire [SIZE -1 : 0 ]D, Q, QN;
 
    write_demux write_demux(clk, write, write_reg, out_write_demux);
-   
-   //32 registres de 32 bits
-   wire [1023:0]D, Q, QN;
-   //a faire //repliquer datain sur l'input D de chaque latch (sans buffer)
-   //   derivator #(.WAY(WAY), .SIZE(SIZE)) replicator(datain, D);
-   parallel_Dlatch #(.WAY(32), .SIZE(32))block_Dlatch({32{datain}}, out_write_demux, Q, QN);
+
+   replicator #(.WAY(NB_REG), .WIRE(SIZE_REG)) replicator(D, datain);
+
+   parallel_Dlatch #(.WAY(NB_REG), .WIRE(SIZE_REG))block_Dlatch(D, out_write_demux, Q, QN);
 
    read_mux read_A(read_A, Q, data_out_A);
    read_mux read_B(read_B, Q, data_out_B);
