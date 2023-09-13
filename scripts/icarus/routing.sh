7a9fc4c54854a843227ca878a2f9fe3851402bd6
alcim jeremy <alcim.dev@gmail.com>
2023-09-13 10:33:11 +0200

    del decLR

--- scripts/icarus/routing.sh
+++ scripts/icarus/routing.sh
@@ -7,7 +7,7 @@ test_routing()
 
     mkdir -p $BIN_routing
 
-    iverilog -o $BIN_routing/test_decalleur_LR $TEST_routing/test_decalleur_LR.v
+#    iverilog -o $BIN_routing/test_decalleur_LR $TEST_routing/test_decalleur_LR.v
 }
 
 test_routing
