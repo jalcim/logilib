3743c7bfe1620a136a310ff16805bd7795dba754
ghost <ghost@raven>
2023-09-21 13:40:45 +0200

    cmp

--- src/alu/arithm/cmp.v
+++ src/alu/arithm/cmp.v
@@ -1,3 +1,10 @@
+`ifndef __CMP__
+ `define __CMP__
+
+ `include "src/primitive/parallel_gate/parallel_nor.v"
+ `include "src/primitive/complex_gate/complex_gate.v"
+ `include "src/primitive/parallel_gate/parallel_and.v"
+
 module cmp_cell(e1, e2, left, right);
    input e1, e2;
    output left, right;
@@ -25,11 +32,29 @@ endmodule
 module cmp(a, b, eq, less, more);
    parameter WIRE = 8;
    input [WIRE-1:0] a, b;
-   wire [WIRE-1:0]  left, right;
-   wire [WIRE-1:0]  out_nor;
+   wire [WIRE-1:0]  left, right, out_nor;
+   wire [WIRE-3:0]  out_ao;//, out_buf_and;
+
+   wire		    out_and;
 
    parallel_cmp_cell #(.WIRE(WIRE)) parallel_cmp_cell_inst(a, b, left, right);
-
    parallel_gate_nor #(.WAY(2), .WIRE(WIRE)) parallel_gate_nor_inst(out_nor, left, right);
-   
+
+   //ao x7
+   gate_ao ao_inst1(out_ao[0], left[0]  , out_nor[1], left[1]);
+   gate_ao ao_inst2(out_ao[1], out_ao[0], out_nor[2], left[2]);
+   gate_ao ao_inst3(out_ao[2], out_ao[1], out_nor[3], left[3]);
+   gate_ao ao_inst4(out_ao[3], out_ao[2], out_nor[4], left[4]);
+   gate_ao ao_inst5(out_ao[4], out_ao[3], out_nor[5], left[5]);
+   gate_ao ao_inst6(out_ao[5], out_ao[4], out_nor[6], left[6]);
+   gate_ao ao_inst7(out_ao[6], out_ao[5], out_nor[7], left[7]);
+
+//   parallel_buf #(.WIRE(WIRE)) buf_and_inst(out_buf_and, out_nor);
+   serial_and #(.WAY(WIRE), .WIRE(1)) and_inst(out_and, out_nor);//out_buf_and);
+
+   nor nor_inst(eq, out_ao[6], out_nor);
+   buf buf_inst_less(less, out_and);
+   buf buf_inst_more(more, out_ao[6]);
 endmodule
+
+`endif
--- src/alu/arithm/cmp8.v
+++ src/alu/arithm/cmp8.v
@@ -24,21 +24,30 @@ module cmp8(a, b, i, j, k);
    gate_nor nor7(left[6], right[6], out_nor[6]);
    gate_nor nor8(left[7], right[7], out_nor[7]);
 
-   gate_and and1(left[0] , out_nor[1], line3[0]);
-   gate_and and2(line3[1] , out_nor[2], line3[2]);
-   gate_and and3(line3[3] , out_nor[3], line3[4]);
-   gate_and and4(line3[5] , out_nor[4], line3[6]);
-   gate_and and5(line3[7] , out_nor[5], line3[8]);
-   gate_and and6(line3[9] , out_nor[6], line3[10]);
-   gate_and and7(line3[11], out_nor[7], line3[12]);
+   //////////////////////
 
+   gate_and and1(left[0] , out_nor[1], line3[0]);
    gate_or or1(line3[0] , left[1] , line3[1]);
+
+   gate_and and2(line3[1] , out_nor[2], line3[2]);
    gate_or or2(line3[2] , left[2] , line3[3]);
+
+   gate_and and3(line3[3] , out_nor[3], line3[4]);
    gate_or or3(line3[4] , left[3] , line3[5]);
+
+   gate_and and4(line3[5] , out_nor[4], line3[6]);
    gate_or or4(line3[6] , left[4] , line3[7]);
+
+   gate_and and5(line3[7] , out_nor[5], line3[8]);
    gate_or or5(line3[8] , left[5], line3[9]);
+
+   gate_and and6(line3[9] , out_nor[6], line3[10]);
    gate_or or6(line3[10], left[6], line3[11]);
+
+   gate_and and7(line3[11], out_nor[7], line3[12]);
    gate_or or7(line3[12], left[7], line3[13]);
+
+   ///////////////////////
 
    multigate_and8 and81(out_nor, line3[14]);
    gate_nor nor9(line3[13], line3[14], j);
