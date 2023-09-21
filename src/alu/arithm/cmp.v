9e5b651b6c9ce34bba987a99ff6fac2b41f7b651
ghost <ghost@raven>
2023-09-21 20:53:03 +0200

    cmp scallable

--- src/alu/arithm/cmp.v
+++ src/alu/arithm/cmp.v
@@ -29,6 +29,21 @@ module parallel_cmp_cell(a, b, left, right);
      parallel_cmp_cell #(.WIRE(WIRE-1)) parallel_cmp_cell_recall(a[WIRE-1:1], b[WIRE-1:1], left[WIRE-1:1], right[WIRE-1:1]);
 endmodule
 
+module recursive_ao(out, e1, e2, e3);
+   parameter WIRE = 8;
+
+   input	  e1;
+   input [WIRE-2:0] e2, e3;
+   output	    out;
+   wire		    line;
+
+   gate_ao ao_inst1(line, {e1, e2[0], e3[0]});
+   if (WIRE > 2)
+     recursive_ao #(.WIRE(WIRE-1)) recall(out, line, e2[WIRE-2:1], e3[WIRE-2:1]);
+   else
+     assign out = line;
+endmodule
+
 module cmp(a, b, eq, more, less);
    parameter WIRE = 8;
    input [WIRE-1:0] a, b;
@@ -36,27 +51,23 @@ module cmp(a, b, eq, more, less);
    output	    eq, less,more;
 
    wire [WIRE-1:0]  left, right, out_nor;
-   wire [WIRE-2:0]  out_ao;//, out_buf_and;
+
    wire		    out_and;
+   wire		    out_ao;
 
    parallel_cmp_cell #(.WIRE(WIRE)) parallel_cmp_cell_inst(a, b, left, right);
    parallel_nor #(.WAY(2), .WIRE(WIRE)) parallel_nor_inst(out_nor, {left, right});
 
-   //ao x7
-   gate_ao ao_inst1(out_ao[0], {left[0]  , out_nor[1], left[1]});
-   gate_ao ao_inst2(out_ao[1], {out_ao[0], out_nor[2], left[2]});
-   gate_ao ao_inst3(out_ao[2], {out_ao[1], out_nor[3], left[3]});
-   gate_ao ao_inst4(out_ao[3], {out_ao[2], out_nor[4], left[4]});
-   gate_ao ao_inst5(out_ao[4], {out_ao[3], out_nor[5], left[5]});
-   gate_ao ao_inst6(out_ao[5], {out_ao[4], out_nor[6], left[6]});
-   gate_ao ao_inst7(out_ao[6], {out_ao[5], out_nor[7], left[7]});
+   recursive_ao #(.WIRE(WIRE)) ao_inst(out_ao,
+				       left[0],
+				       out_nor[WIRE-1:1],
+				       left[WIRE-1:1]);
 
-//   parallel_buf #(.WIRE(WIRE)) buf_and_inst(out_buf_and, out_nor);
-   serial_and #(.WAY(WIRE)) and_inst(out_and, out_nor);//out_buf_and);
+   serial_and #(.WAY(WIRE)) and_inst(out_and, out_nor);
 
-   nor nor_inst(more, out_ao[6], out_and);
+   nor nor_inst(more, out_ao, out_and);
    buf buf_inst_less(eq, out_and);
-   buf buf_inst_more(less, out_ao[6]);
+   buf buf_inst_more(less, out_ao);
 endmodule
 
 `endif
