ea252633684804e2b962db5d8fc409a1106e3d07
ghost <ghost@raven>
2023-09-23 12:51:45 +0200

    waiting rename WAY WIRE mux demux

--- exemple/block_mem/v2/blockreg.v
+++ exemple/block_mem/v2/blockreg.v
@@ -2,7 +2,7 @@
  `define __BLOCKREG__
 
  `include "src/routing/replicator.v"
- `include "src/memory/Dlatch/parallel_Dlatch.v"
+ `include "src/memory/Dlatch/parallel_Dlatch_rst.v"
 
 module blockreg(clk, reset, write,
 		addr_write_reg, read_A, read_B,
@@ -16,20 +16,20 @@ module blockreg(clk, reset, write,
 
    input      clk, reset, write;
    input [SIZE_ADDR_REG-1:0] addr_write_reg, read_A, read_B;
-   input [SIZE_REG:0]	     datain;
+   input [SIZE_REG-1:0]	     datain;
 
    output [SIZE_REG-1:0]     data_out_A, data_out_B;
 
-   wire			     out_write_demux;
+   wire [NB_REG-1:0]	     out_write_demux;
    wire [SIZE -1 : 0 ]	     D, Q, QN;
 
    write_demux #(.SIZE_ADDR_REG(SIZE_ADDR_REG)) write_demux(clk, addr_write_reg, write, out_write_demux);
 
    replicator #(.WAY(NB_REG), .WIRE(SIZE_REG)) replicator(D, datain);
-   parallel_Dlatch #(.WAY(NB_REG), .WIRE(SIZE_REG))block_Dlatch(D, out_write_demux, Q, QN);
+   parallel_Dlatch_rst #(.WAY(NB_REG), .WIRE(SIZE_REG))block_Dlatch(D, out_write_demux, {NB_REG{reset}}, Q, QN);
 
-   read_mux #(.SIZE_ADDR_REG(SIZE_ADDR_REG)) read_mux_A(read_A, Q, data_out_A);
-   read_mux #(.SIZE_ADDR_REG(SIZE_ADDR_REG)) read_mux_B(read_B, Q, data_out_B);
+   read_mux #(.SIZE_ADDR_REG(SIZE_ADDR_REG), .SIZE_REG(SIZE_REG)) read_mux_A(read_A, Q, data_out_A);
+   read_mux #(.SIZE_ADDR_REG(SIZE_ADDR_REG), .SIZE_REG(SIZE_REG)) read_mux_B(read_B, Q, data_out_B);
 
 endmodule
 
@@ -56,7 +56,7 @@ module read_mux(read_addr, out_reg, out_read_mux);
    parameter SIZE_ADDR_REG = 5;
 
    input [SIZE_ADDR_REG-1:0] read_addr;
-   input [2**SIZE_ADDR_REG:0] out_reg;
+   input [(2**SIZE_ADDR_REG)-1:0] out_reg;
    output 	    out_read_mux;
 
    mux #(.S(SIZE_ADDR_REG)) mux0(read_addr, out_reg, out_read_mux);
