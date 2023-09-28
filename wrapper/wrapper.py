c3f1a8995798325ce24712e8fcb745c4cfeaeb85
ghost <ghost@raven>
2023-09-28 14:57:56 +0200

    feat(python wrapper) topmodule_and_submodule_and_relied

--- wrapper/wrapper.py
+++ wrapper/wrapper.py
@@ -52,27 +52,34 @@ class Module():
 
     module_id = 0 # auto increment
 
-    def __init__(self, module_type, params):
-        if module_type == "gate_or":
+    def __init__(self, module_type, params, allocate):
+        if allocate == 1:
+            if module_type == "gate_or":
+                self.params = {
+                    "i_in" : am.Signal(params["WAY"] * params["WIRE"]),
+                    "o_out": am.Signal(params["WIRE"]),
+                }
+            elif module_type == "gate_and":
+                self.params = {
+                    "i_in" : am.Signal(params["WAY"] * params["WIRE"]),
+                    "o_out": am.Signal(params["WIRE"]),
+                }
+            elif module_type == "gate_nand":
+                self.params = {
+                    "i_in" : am.Signal(params["WAY"] * params["WIRE"]),
+                    "o_out": am.Signal(params["WIRE"]),
+                }
+            elif module_type == "gate_not":
+                self.params = {
+                    "i_in" : am.Signal(params["WIRE"]),
+                    "o_out": am.Signal(params["WIRE"]),
+                }
+        else :
             self.params = {
-                "i_in" : am.Signal(params["WAY"] * params["WIRE"]),
+                "i_in" : 0,
                 "o_out": am.Signal(params["WIRE"]),
             }
-        elif module_type == "gate_and":
-            self.params = {
-                "i_in" : am.Signal(params["WAY"] * params["WIRE"]),
-                "o_out": am.Signal(params["WIRE"]),
-            }
-        elif module_type == "gate_nand":
-            self.params = {
-                "i_in" : am.Signal(params["WAY"] * params["WIRE"]),
-                "o_out": am.Signal(params["WIRE"]),
-            }
-        elif module_type == "gate_not":
-            self.params = {
-                "i_in" : am.Signal(params["WIRE"]),
-                "o_out": am.Signal(params["WIRE"]),
-            }
+
         self.params.update({"p_" + key: value for key, value in params.items() if key in ALLOWED_PARAMS[module_type]})
         self.module = Verilog_module(
             module_type,
@@ -109,18 +116,51 @@ def write_rtlil_file(modules_list : list):
             print(f"'{elem.module.rtlil_file}' filename written.")
 
 
+def write_top_rtlil(top : am.Elaboratable):
+    name = "top"
+    platform = None
+    emit_src = True
+    strip_internal_attrs = False
+    fragment = Fragment.get(top, platform).prepare(ports=top.ports)
+    rtlil_text, name_map = convert_fragment(
+        fragment,
+        name,
+        emit_src=emit_src,
+    )
+    ENV_USERNAME = os.environ.get("USER")
+    rtlil_source_text = rtlil_text.replace(ENV_USERNAME, "user")
+    return rtlil_source_text
+
+class Top(am.Elaboratable):
+    def __init__(self):
+        self.ports = []
+
+    def elaborate(self, platform):
+        self.gate1 = Module("gate_and", {"WAY": 2, "WIRE": 1}, 1)
+        self.gate2 = Module("gate_and", {"WAY": 2, "WIRE": 1}, 1)
+        self.gate3 = Module("gate_and", {"WAY": 2, "WIRE": 1}, 0)
+        self.gate3.set("i_in", am.Cat(self.gate1.get("o_out"), self.gate2.get("o_out")))
+
+        top = am.Module()
+        top.submodules.gate_and1 = self.gate1.module
+        top.submodules.gate_and2 = self.gate2.module
+        top.submodules.gate_and3 = self.gate3.module
+
+        self.ports.append(self.gate1.module.ports)
+        self.ports.append(self.gate2.module.ports)
+        self.ports.append(self.gate3.module.ports)
+
+        return top
+
 if __name__ == "__main__":
 
     # exemple :
-    module_1 = Module("gate_not", {"WIRE": 1})
-    module_2 = Module("gate_or", {"WAY": 2, "WIRE": 1})
-    module_3 = Module("gate_and", {"WAY": 2, "WIRE": 1})
-    module_4 = Module("gate_nand", {"WAY": 2, "WIRE": 1, "BEHAVIORAL": 1})
+    module_1 = Module("gate_not" , {"WIRE": 1}, 1)
+    module_2 = Module("gate_or"  , {"WAY": 2, "WIRE": 1}, 1)
+    module_3 = Module("gate_nand", {"WAY": 2, "WIRE": 1, "BEHAVIORAL": 1}, 1)
+    module_4 = Module("gate_and", {"WAY": 2, "WIRE": 1}, 1)
 
-    module_1.set("o_out", module_2.get("i_in"))
-    module_2.set("o_out", module_3.get("i_in"))
-    module_3.set("o_out", module_4.get("i_in"))
-    module_4.set("p_WIRE", 1)
+    module_1.set("p_WIRE", 1)
 
     modules_list = [
         module_1,
@@ -129,3 +169,7 @@ if __name__ == "__main__":
         module_4
     ]
     write_rtlil_file(modules_list)
+
+    top = Top()
+    write_top_rtlil(top)
+
