7a2d7155afe099ebfb248805e1d4bd8b9266b815
jalcim <ghost@raven>
2025-01-23 22:00:49 +0100

    rename some signal on test_block_reg

--- exemple/block_reg/test_block_reg.v
+++ exemple/block_reg/test_block_reg.v
@@ -4,51 +4,51 @@ module test_block_reg;
    parameter SIZE_ADDR_REG = 5;
    parameter SIZE_REG = 8;
 
-   reg	     clk, reset, charge;
-   reg [SIZE_ADDR_REG-1:0] addr_write_reg, outA, outB;
+   reg	     clk, reset, write_rd;
+   reg [SIZE_ADDR_REG-1:0] rd, rs1, rs2;
    reg [SIZE_REG-1:0] datain;
 
-   wire [SIZE_REG-1:0] dataoutA, dataoutB;
+   wire [SIZE_REG-1:0] out_rs1, out_rs2;
 
-   block_reg #(.SIZE_ADDR_REG(SIZE_ADDR_REG), .SIZE_REG(SIZE_REG))block_reg(clk, reset, charge,
-									  addr_write_reg, outA, outB,
-									  datain,
-									  dataoutA, dataoutB);
+   block_reg #(.SIZE_ADDR_REG(SIZE_ADDR_REG), .SIZE_REG(SIZE_REG))block_reg(clk, reset, write_rd,
+									    rd, rs1, rs2,
+									    datain,
+									    out_rs1, out_rs2);
 
    initial
      begin
 	$dumpfile("signal_block_reg.vcd");
 	$dumpvars;
-	$display("\t\ttime, \tclk, \treset, \tcharge, \taddr_write_reg, \toutA, \toutB, \tdatain, \tdataoutA, \tdataoutB\n");
+	$display("\t\ttime, \tclk, \treset, \twrite_rd, \trd, \t\t\trs1, \trs2, \tdatain, \tout_rs1, \tout_rs2\n");
 	$monitor("%d \t%b \t%b \t%b \t\t%d \t\t\t%d \t%d \t%d \t\t%d \t\t%d \n",
-		 $time, clk, reset, charge, addr_write_reg, outA, outB, datain, dataoutA, dataoutB);
+		 $time, clk, reset, write_rd, rd, rs1, rs2, datain, out_rs1, out_rs2);
 
 	$display("init");
 	clk = 0;
 	reset = 1;
-	charge = 0;
-	outA = 0;
-	outB = 0;
-	addr_write_reg = 0;
+	write_rd = 0;
+	rs1 = 0;
+	rs2 = 0;
+	rd = 0;
 	datain = 0;
 	#100;
 
-	$display("on prepare la charge mais pas de clk");
+	$display("on prepare la write_rd mais pas de clk");
 	reset = 0;
 	datain = 7;
-	addr_write_reg = 2;
-	charge = 1;
-	outA = 2;
+	rd = 2;
+	write_rd = 1;
+	rs1 = 2;
 	#100;
 
-	$display("on charge 7 dans 2");
+	$display("on write_rd 7 dans 2");
 	clk = 1;
 	#100;
 
 	$display("on enleve les signaux");
 	clk = 0;
-	charge = 0;
-	addr_write_reg = 0;
+	write_rd = 0;
+	rd = 0;
 	datain = 0;
 	#100;
 	
@@ -60,29 +60,29 @@ module test_block_reg;
 	clk = 0;
 	#100;
 
-	$display("charge sans clk");
-	charge = 1;
+	$display("write_rd sans clk");
+	write_rd = 1;
 	#100;
 
-	$display("clk sans charge");
+	$display("clk sans write_rd");
 	clk = 1;
-	charge = 0;
+	write_rd = 0;
 	#100;
 
-	$display("charge 0 dans 2");
+	$display("write_rd 0 dans 2");
 	clk = 1;
-	charge = 1;
-	addr_write_reg = 2;
+	write_rd = 1;
+	rd = 2;
 	#100;
 
 	$display ("enleve clk");
 	clk = 0;
 	#100
 
-	$display("charge 250 dans 2");
+	$display("write_rd 250 dans 2");
 	clk = 1;
-	charge = 1;
-	addr_write_reg = 2;
+	write_rd = 1;
+	rd = 2;
 	datain = 250;
 	#100;
 
@@ -90,12 +90,12 @@ module test_block_reg;
 	clk <= 0;
 	#100
 
-	$display("charge 124 dans 30");
+	$display("write_rd 124 dans 30");
 	clk = 1;
-	charge = 1;
-	addr_write_reg = 30;
+	write_rd = 1;
+	rd = 30;
 	datain = 124;
-	outB = 30;
+	rs2 = 30;
 	#100;
 
      end
