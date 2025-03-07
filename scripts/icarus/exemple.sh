e73ec6d9ce181bddb21c402123a6534523a4bd7c
jalcim <alcim.dev@gmail.com>
2025-03-07 15:04:37 +0100

    exemple script

--- scripts/icarus/exemple.sh
+++ scripts/icarus/exemple.sh
@@ -1,5 +1,36 @@
+#!/bin/bash
+
 mkdir -p bin/exemple
 
-iverilog -pRECURSIVE_MOD_LIMIT=100 -o bin/exemple/test_block_reg exemple/block_reg/test_block_reg.v
-iverilog -pRECURSIVE_MOD_LIMIT=100 -o bin/exemple/test_block_mem exemple/block_mem/test_block_mem.v
+echo "test_bru"
+iverilog -pRECURSIVE_MOD_LIMIT=100 -o bin/exemple/test_bru       exemple/alu/bru/test_bru.v
+echo "test_alu"
 iverilog -pRECURSIVE_MOD_LIMIT=100 -o bin/exemple/test_alu       exemple/alu/test_alu.v
+
+echo "test_block_reg"
+iverilog -pRECURSIVE_MOD_LIMIT=100 -o bin/exemple/test_block_reg exemple/block_reg/test_block_reg.v
+
+echo "test_decodeur"
+iverilog -pRECURSIVE_MOD_LIMIT=100 -o bin/exemple/test_decodeur  exemple/decodeur/test_decodeur.v
+
+echo "test_block_mem"
+iverilog -pRECURSIVE_MOD_LIMIT=100 -o bin/exemple/test_block_mem exemple/lsu/block_mem/test_block_mem.v
+#echo "test_lsu"
+#iverilog -pRECURSIVE_MOD_LIMIT=100 -o bin/exemple/test_lsu       exemple/lsu/test_lsu.v
+echo "test_mux_load"
+iverilog -pRECURSIVE_MOD_LIMIT=100 -o bin/exemple/test_mux_load  exemple/lsu/test_mux_load.v
+echo "test_mux_store"
+iverilog -pRECURSIVE_MOD_LIMIT=100 -o bin/exemple/test_mux_store exemple/lsu/test_mux_store.v
+
+echo "test_pc"
+iverilog -pRECURSIVE_MOD_LIMIT=100 -o bin/exemple/test_pc exemple/pc/test_pc.v
+
+echo "test_rom"
+iverilog -pRECURSIVE_MOD_LIMIT=100 -o bin/exemple/test_rom exemple/rom/test_rom.v
+
+echo "test_write_back"
+iverilog -pRECURSIVE_MOD_LIMIT=100 -o bin/exemple/test_write_back exemple/write_back/test_write_back.v
+
+
+
+
