aca513a712fcd971bb14b48736239be6c821b1b3
jalcim <alcim.dev@gmail.com>
2025-04-06 00:23:19 +0200

    fix_sra_srl

--- src/alu/logic/sra_srl.v
+++ src/alu/logic/sra_srl.v
@@ -8,7 +8,7 @@ module sra_srl(funct7, datain_A, datain_B, out);
    output [WIRE-1:0] out;
 
    assign out = funct7 ?
-		{1'b1, (datain_A[WIRE-2:0] >> datain_B[5:0])}
+		{7'b1, (datain_A[WIRE-2:0] >> datain_B[5:0])}
 		:
 		datain_A >> datain_B[5:0];
 endmodule
