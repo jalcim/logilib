89234d3a8eaabf728c7323b2418f17bcdd11ffa7
jalcim <alcim.dev@gmail.com>
2025-04-07 12:47:24 +0200

    modif after rebase

--- wrapper/amaranth/test/test_wrapper2.py
+++ wrapper/amaranth/test/test_wrapper2.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import amaranth as am
 from Module import Module
 from Top import Top
