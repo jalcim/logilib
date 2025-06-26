a98773fc033cc83c43343fb99d8cd018fcbbaf59
jalcim <alcim.dev@gmail.com>
2025-06-26 16:52:52 +0200

    fix dlatch

--- src/memory/dlatch/Dlatch.v
+++ src/memory/dlatch/Dlatch.v
@@ -4,7 +4,6 @@
  `include "src/memory/dlatch/parallel/parallel_Dlatch.v"
  `include "src/memory/dlatch/serial/serial_Dlatch.v"
 
-/* verilator lint_off UNOPTFLAT */
 module Dlatch (D, clk, Q, QN);
    parameter WAY = 1;
    parameter WIRE = 1;
@@ -13,30 +12,16 @@ module Dlatch (D, clk, Q, QN);
    input [(WAY * WIRE)-1 : 0] D;
    output [(WAY * WIRE)-1:0]  Q, QN;
 
-   wire [4:0] line;
-
    if (WAY > 1)
      parallel_Dlatch #(.WAY(WAY), .WIRE(WIRE)) parallel_Dlatch_inst(.D(D),
 								    .clk(clk),
 								    .Q(Q),
 								    .QN(QN));
-   else if (WIRE > 1)
+   else
      serial_Dlatch #(.WIRE(WIRE)) inst0(.D(D),
 					.clk(clk),
 					.Q(Q),
 					.QN(QN));
-   else
-     begin
-	not not0(line[0], clk);
-	nor nor0(line[1], D, line[0]);
-	nor nor1(line[2], line[1], line[4]);
-
-	and and2(line[3], D, clk);
-	nor nor3(line[4], line[3], line[2]);
-
-	assign Q  = line[2];
-	assign QN = line[4];
-     end
 
 endmodule
 
--- src/memory/dlatch/serial/serial_Dlatch.v
+++ src/memory/dlatch/serial/serial_Dlatch.v
@@ -1,8 +1,6 @@
 `ifndef __SERIAL_DLATCH__
  `define __SERIAL_DLATCH__
 
- `include "src/memory/dlatch/Dlatch.v"
-
 module serial_Dlatch(D, clk, Q, QN);
    parameter WIRE = 1;
 
@@ -10,10 +8,21 @@ module serial_Dlatch(D, clk, Q, QN);
    input [WIRE -1:0] D;
    output [WIRE -1:0] Q, QN;
 
-   Dlatch latch1(.D(D[0]),
-		 .clk(clk),
-		 .Q(Q[0]),
-		 .QN(QN[0]));
+   /* verilator lint_off UNOPTFLAT */
+   wire [4:0] line;
+
+   /* verilator lint_off UNOPTFLAT */
+   wire	      line2;
+
+   not not0(line[0], clk);
+   nor nor0(line[1], D[0], line[0]);
+   nor nor1(line2, line[1], line[4]);
+   and and2(line[3], D[0], clk);
+   nor nor3(line[4], line[3], line2);
+
+   assign Q[0]  = line2;
+   assign QN[0] = line[4];
+
    if (WIRE > 1)
      serial_Dlatch #(.WIRE(WIRE-1)) recall(.D(D[WIRE-1:1]),
 					   .clk(clk),
