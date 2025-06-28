312bd8f90aa8c2b36d433df33c411d91fe6ed86c
jalcim <alcim.dev@gmail.com>
2025-06-28 21:58:54 +0200

    fix_sltu

--- src/alu/logic/sltu.v
+++ src/alu/logic/sltu.v
@@ -6,7 +6,7 @@ module sltu(datain_A, datain_B, out);
    input [WIRE-1:0] datain_A, datain_B;
    output [WIRE-1:0] out;
 
-   assign out = datain_A < datain_B;
+   assign out[0] = datain_A < datain_B;
    assign out[WIRE-1:1] = 0;
 endmodule
 
